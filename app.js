45e6c384d7003a1225f9dcefedd2cfe5dcef06b8
chrismoffat-teamalt <40431956+chrismoffat-teamalt@users.noreply.github.com>
2018-06-20 23:21:30 -0400

    commkt

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ bot.dialog('/', [
             // This would be pulled from OLB database/datalake
             session.send("Please verify the following..." + 
                         " \r\n " + 
-                        " \r\n First Name:          Team" +
+                        " \r\n First Name:\t\t\t    Team" +
                         " \r\n Last Name:           ALT" +
                         " \r\n Client Card No:      5555 5555 5555 5555" +
                         " \r\n Occupation:          RBC" +
